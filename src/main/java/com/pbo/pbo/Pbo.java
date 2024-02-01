46bdea939195306df811aedbd899706ef072537b
cikalrpratama <crafapratama@gmail.com>
2024-02-01 10:36:44 +0700

    praktikum pbo - java- commit pertama kali

---
+++ src/main/java/com/pbo/pbo/Pbo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pbo.pbo;
+
+/**
+ *
+ * @author cikal r pratama
+ */
+public class Pbo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
